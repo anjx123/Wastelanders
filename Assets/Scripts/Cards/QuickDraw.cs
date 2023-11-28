9416fc5e0578f97b00072d294414020d80e93ee5
anjx123 <114792511+anjx123@users.noreply.github.com>
2023-11-28 11:06:54 -0800

    Card Prefab Added for consistency
    Card Prefab Added for consistency
    
    Slime Stack animations imported

---
+++ Assets/Scripts/Cards/QuickDraw.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickDraw : ActionClass
+{
+    public override void ExecuteActionEffect()
+    {
+        Debug.Log("Executing Quick Draw");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
