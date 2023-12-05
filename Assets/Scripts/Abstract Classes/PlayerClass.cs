17166c509b1ae764d331fb8adca6813f2699282d
Andrew Wang <andrewzlwang@outlook.com>
2023-12-04 21:46:44 -0800

    implemented random drawing

--- Assets/Scripts/Abstract Classes/PlayerClass.cs
+++ Assets/Scripts/Abstract Classes/PlayerClass.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerClass : EntityClass
 {
     public List<GameObject> cardPrefabs;
+    public RectTransform handContainer;
+    public int cardWidth;
 
-    private int maxHandSize = 5;
-
+    private int maxHandSize = 2;
+    private int currentHandSize = 0;
     // This is the deck of the player, which does not change as the player reshuffles/draws cards.
     private List<GameObject> deck = new List<GameObject>();
 
@@ -23,16 +25,30 @@ public class PlayerClass : EntityClass
     // Start is called before the first frame update
     void Start()
     {
+        // initialize deck
         for (int i = 0; i < cardPrefabs.Count; i++)
         {
             GameObject toAdd = Instantiate(cardPrefabs[i]);
             deck.Add(toAdd);
+            toAdd.transform.position = new Vector3(1000, 1000, 1);
         }
-        pool = deck;
+
+        // deep copy deck into pool
+        for (int i = 0; i < cardPrefabs.Count; i++)
+        {
+            GameObject toAdd = Instantiate(deck[i]);
+            pool.Add(toAdd);
+            toAdd.transform.position = new Vector3(-1000, -1000, 1);
+        }
+        
+        Debug.Log(pool.Count);
         for (int i = 0; i < maxHandSize; i++)
         {
             DrawCard();
         }
+
+        Debug.Log(hand.Count);
+        RenderHand();
     }
 
 
@@ -48,9 +64,33 @@ public class PlayerClass : EntityClass
             maxHandSize--;
             pool = deck;
         }
-        int idx = Random.Range(0, pool.Count - 1);
+        int idx = Random.Range(0, pool.Count);
         hand.Add(pool[idx]);
+        currentHandSize++;
         pool.RemoveAt(idx);
+    }
+
+    /*
+     * 
+     * 
+     * 
+     */
+    void RenderHand()
+    {
+        for (int i = 0; i < hand.Count; i++)
+        {
+            Debug.Log(i);
+            hand[i].transform.SetParent(handContainer.transform, false);
+            hand[i].transform.position = Vector3.zero;
+
+            float distanceToLeft = handContainer.rect.width / 2 - (i * cardWidth);
+
+            float y = handContainer.transform.position.y;
+            Vector3 v = new Vector3(-distanceToLeft, y, 1);
+            hand[i].transform.position = v;
+            Debug.Log(hand[i].transform.position);
+
+        }
     }
 
     // Update is called once per frame
