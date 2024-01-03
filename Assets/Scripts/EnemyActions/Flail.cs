d1681aff772bd5e3bdce4bcf7872b642c56adb73
Andrew Wang <andrewzlwang@outlook.com>
2024-01-03 00:25:44 -0800

    added another attack

---
+++ Assets/Scripts/EnemyActions/Flail.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flail : ActionClass
+{
+	public Flail()
+	{
+	}
+
+    public override void ExecuteActionEffect()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Start()
+    {
+        Speed = 2;
+        Block = 4;
+        Damage = 3;
+        myName = "Flail";
+    }
+}
+
