f60d4f15a5e76e406cba4c24615a31fe441f870f
anjx123 <114792511+anjx123@users.noreply.github.com>
2024-01-29 23:11:16 -0800

    Hard Coded Slime Animations
    Not the prettiest solution but the solution that gives the most control when programming animations and so I am willing to sacrifice this sadly

--- Assets/Scripts/Cards/EnemyCards/SlimeCards/Pound.cs
+++ Assets/Scripts/Cards/EnemyCards/SlimeCards/Pound.cs
@@ -7,6 +7,8 @@ using static UnityEngine.UI.Image;
 
 public class Pound : SlimeAttacks
 {
+    [SerializeField]
+    private List<Sprite> animationFrame = new();
     public override void ExecuteActionEffect()
     {
 
@@ -41,7 +43,28 @@ public class Pound : SlimeAttacks
 
     public override void OnHit()
     {
+        StartCoroutine(AttackAnimation());
+        //Origin.AttackAnimation("IsPounding");
+    }
+
+
+    public IEnumerator AttackAnimation()
+    {
+        Vector3 originalPosition = Origin.myTransform.position;
+        Origin.animator.enabled = false;
+        SpriteRenderer spriteRenderer = Origin.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = animationFrame[0];
+        Origin.myTransform.position = originalPosition;
+       yield return new WaitForSeconds(0.05f);
+        spriteRenderer.sprite = animationFrame[1];
         base.OnHit();
-        Origin.AttackAnimation("IsPounding");
+        yield return new WaitForSeconds(0.010f);
+        spriteRenderer.sprite = animationFrame[2];        
+        yield return new WaitForSeconds(0.32f);
+        spriteRenderer.sprite = animationFrame[3];
+        yield return new WaitForSeconds(0.20f);
+        spriteRenderer.sprite = animationFrame[4];
+        Origin.animator.enabled = true;
+        Origin.myTransform.position = originalPosition;
     }
 }
