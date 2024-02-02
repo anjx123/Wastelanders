9f04d4631c7526685c1d6d2b662238d400687136
anjx123 <114792511+anjx123@users.noreply.github.com>
2024-02-01 23:54:37 -0800

    Deleted Warnings

--- Assets/Scripts/Managers/CardComparator.cs
+++ Assets/Scripts/Managers/CardComparator.cs
@@ -24,7 +24,6 @@ public class CardComparator : MonoBehaviour
             Destroy(this);
         }
 
-        DontDestroyOnLoad(gameObject);
     }
 
     /*
--- Assets/Scripts/Managers/CombatManager.cs
+++ Assets/Scripts/Managers/CombatManager.cs
@@ -33,7 +33,6 @@ public class CombatManager : MonoBehaviour
             Destroy(this);
         }
 
-        DontDestroyOnLoad(gameObject);
     }
 
     // Start is called before the first frame update
