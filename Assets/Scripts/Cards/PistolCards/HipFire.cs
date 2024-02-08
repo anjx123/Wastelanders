00cfa7e652315fa257d41e10aa60f04d7fa50867
Andrew Wang <andrewzlwang@outlook.com>
2024-02-08 13:03:45 -0800

    adeed two more cards

--- Assets/Scripts/Cards/PistolCards/HipFire.cs
+++ Assets/Scripts/Cards/PistolCards/HipFire.cs
@@ -1,52 +1,46 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Silencer : PistolCards
+public class HipFire : PistolCards
 {
+
+    
     public override void ExecuteActionEffect()
     {
-
+        
     }
 
     // Start is called before the first frame update
     public override void Initialize()
     {
         lowerBound = 1;
-        upperBound = 4;
-        Speed = 3;
+        upperBound = 5;
+        Speed = 1;
         Block = 2;
-        description = "If this Attack Staggers the opponent, gain 2 accuracy.";
-        myName = "Silencer";
-        CardType = CardType.RangedAttack;
+        Damage = 3;
+        description = "Make this attack once, if unstaggered, make it again.";
+        CardType = CardType.MeleeAttack;
+        myName = "HipFire";
         Renderer renderer = GetComponent<Renderer>();
         ogMaterial = renderer.material; // og sprite of card
         OriginalPosition = transform.position;
         base.Initialize();
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public override void ApplyEffect()
     {
-
         DupInit();
-
         
         Origin.ApplyAllBuffsToCard(ref duplicateCard);
-
     }
 
-    public override void OnHit()
-    {
-        base.OnHit();
-        Origin.AddStacks(Accuracy.buffName, 2);
 
-    }
+
 }
