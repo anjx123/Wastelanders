89ac6b6da6bc87dd911f3cf687f58b697ea1f66f
anjx123 <114792511+anjx123@users.noreply.github.com>
2024-02-19 01:00:47 -0800

    DialogueManager implemented

---
+++ Assets/Scripts/VNScripts/ActualDialogue/Introduction.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Introduction : MonoBehaviour
+{
+    [SerializeField] private List<DialogueText> list;
+
+    private void Start()
+    {
+    }
+}
--- Assets/Scripts/VNScripts/DialogueManager.cs
+++ Assets/Scripts/VNScripts/DialogueManager.cs
@@ -1,18 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static DialogueManager Instance { get; private set; }
+
+    public GameObject dialogueBoxObj;
+    private DialogueBox dialogueBoxComponent;
+    public List<DialogueText> sentences;
+
+    private bool inDialogue = false;
+
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        dialogueBoxComponent = dialogueBoxObj.GetComponent<DialogueBox>();
+
     }
 
-    // Update is called once per frame
+    //Manager is going to listen to a bunch of events that then cause it to gain new sentences and 
+    void Start()
+    {
+        //clearPanel();
+    }
+
+    public void StartDialogue(List<DialogueText> newSentences)
+    {
+        if (dialogueBoxObj.activeInHierarchy)
+        {
+            return;
+        }
+        Debug.Log("Receiving the new sentence here it is" + newSentences);
+
+        Time.timeScale = 0; //Time freezes
+
+        dialogueBoxObj.SetActive(true);
+
+        sentences = newSentences;
+        inDialogue = true;
+
+        DisplayNextSentence();
+    }
+
+    public void clearPanel()
+    {
+        dialogueBoxObj.SetActive(false);
+        sentences.Clear();
+        Time.timeScale = 1;
+        inDialogue = false;
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        DialogueText sentence = sentences[0];
+        sentences.RemoveAt(0); //Uses a list instead of a Queue so we can see it in the Unity Editor
+
+        dialogueBoxComponent.SetLine(sentence);
+    }
+
+    void EndDialogue()
+    {
+        clearPanel();
+    }
+
     void Update()
     {
-        
+        if (dialogueBoxComponent.FinishedLine())
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKey(KeyCode.Space))
+            {
+                if (inDialogue)
+                {
+                    DisplayNextSentence();
+                }
+            }
+        }
     }
 }
