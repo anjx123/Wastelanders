bf6164a677afb2ab7d9e0a48cfd89b4df63d988b
SneakyGremlin <daudvirk@student.ubc.ca>
2024-02-25 14:38:56 +0500

    found a workaround for RapidFire. WasteFrog is bugged.
    RapidFire creates one card based on the stack it has.
    
    WasteFrog's connection b/w Hurl and Charge Up is severed? Anrui fix...

--- Assets/Scripts/Cards/PistolCards/RapidFire.cs
+++ Assets/Scripts/Cards/PistolCards/RapidFire.cs
@@ -1,11 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RapidFire : PistolCards
 {
-
-    // static int count = 4;
     
     public override void ExecuteActionEffect()
     {
@@ -36,27 +35,35 @@ public class RapidFire : PistolCards
 
     public override void CardIsUnstaggered()
     {
-        /*        Debug.Log(Origin.GetBuffStacks(Accuracy.buffName));
-                Origin.AddStacks(Accuracy.buffName, 10); // for debuggin*/
 
         if (proto && activeDupCardInstance == null)
         {
             activeDupCardInstance = Instantiate(duplicateCardInstance.GetComponent<RapidFireDuplicate>());
             ((RapidFireDuplicate)activeDupCardInstance).proto = false;
             ((RapidFireDuplicate)activeDupCardInstance).duplicateCardInstance = null;
-            activeDupCardInstance.transform.position = new Vector3(-10, 10, 10);
+            activeDupCardInstance.transform.position = new Vector3(-10, -10, -10);
         }
+
+/*        Origin.AddStacks(Accuracy.buffName, 10); // for debuggin
+        Debug.Log(Origin.GetBuffStacks(Accuracy.buffName));*/
 
         if (proto && Origin.GetBuffStacks(Accuracy.buffName) > 0) //&& (count > 0)) // doesn't have to be the original card 
         {
-            // count--;
+            ((RapidFireDuplicate)activeDupCardInstance).Damage = 0; // reset it each time
+            int damageValue = 0;
+            while (Origin.GetBuffStacks(Accuracy.buffName) > 0 && damageValue < 3)
+            {
+                damageValue++;
+                Origin.ReduceStacks(Accuracy.buffName, 1);
+            }
             PlayerClass origin = (PlayerClass)Origin;
             activeDupCardInstance.Origin = origin;
             activeDupCardInstance.Target = Target;
+            ((RapidFireDuplicate)activeDupCardInstance).Damage = damageValue * Damage;
+            // TODO should I update the upper and lower bounds? 
             BattleQueue.BattleQueueInstance.InsertDupPlayerAction(activeDupCardInstance);
-            Origin.ReduceStacks(Accuracy.buffName, 1); // Reduce Accuracy by 1; only once 
-
         }
+
     }
 
 }
