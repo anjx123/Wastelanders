27077de37a8a2daaf6208ffb19e1e77f624cc4d9
anjx123 <114792511+anjx123@users.noreply.github.com>
2024-02-27 23:17:23 -0800

    Ives Dialogue Added to the scene
    Added more dialogue

--- Assets/Scripts/VNScripts/ActualDialogue/TutorialIntroduction.cs
+++ Assets/Scripts/VNScripts/ActualDialogue/TutorialIntroduction.cs
@@ -6,9 +6,12 @@ public class TutorialIntroduction : DialogueClasses
 {
     [SerializeField] private Jackie jackie;
     [SerializeField] private Transform jackieDefaultTransform;
-    [SerializeField] private List<DialogueText> soldierGreeting;
+    [SerializeField] private EnemyIves ives;
+    [SerializeField] private Transform ivesDefaultTransform;
 
-    [SerializeField] private List<DialogueText> dialogueText;
+    [SerializeField] private List<DialogueText> soldierGreeting;
+    [SerializeField] private List<DialogueText> jackieTalksWithSolider;
+    [SerializeField] private List<DialogueText> ivesScoldsJackie;
 
     private IEnumerator ExecuteGameStart()
     {
@@ -16,15 +19,30 @@ public class TutorialIntroduction : DialogueClasses
         yield return new WaitForSeconds(3f);
 
         jackie.SetReturnPosition(jackieDefaultTransform.position);
-        yield return StartCoroutine(jackie.ResetPosition());
+        yield return StartCoroutine(jackie.ResetPosition()); //Jackie Runs into the scene
+
+        yield return new WaitForSeconds(0.2f);
 
         yield return StartCoroutine(DialogueManager.Instance.StartDialogue(soldierGreeting));
 
         yield return new WaitForSeconds(1f);
-        jackie.FaceLeft();
+        jackie.FaceLeft(); //Jackie faces the soldier talking to her
         yield return new WaitForSeconds(0.2f);
 
-        yield return StartCoroutine(DialogueManager.Instance.StartDialogue(dialogueText));
+        yield return StartCoroutine(DialogueManager.Instance.StartDialogue(jackieTalksWithSolider));
+
+        jackie.FaceRight(); //Jackie turns to face the person yelling at her
+
+        yield return new WaitForSeconds(1f);
+
+        ives.SetReturnPosition(ivesDefaultTransform.position);
+        yield return StartCoroutine(ives.MoveToPosition(ivesDefaultTransform.position, 0, 0.8f)); //Ives comes into the scene
+
+        yield return new WaitForSeconds(0.2f);
+
+        yield return StartCoroutine(DialogueManager.Instance.StartDialogue(ivesScoldsJackie));
+
+
 
 
         yield break;
