400ad99fc6140f15cfa13a0cd7909a05565e4907
anjx123 <114792511+anjx123@users.noreply.github.com>
2024-02-27 22:15:18 -0800

    Added A CardUI Script for reusability

--- Assets/Scripts/Entities/Ives.cs
+++ Assets/Scripts/Entities/Ives.cs
@@ -23,16 +23,4 @@ public class Ives : PlayerClass
     {
         throw new System.NotImplementedException();
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
