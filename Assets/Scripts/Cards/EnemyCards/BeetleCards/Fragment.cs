e8e2250f4d387ff53167b1b2830c3ab2d3c87d50
Andrew Wang <andrewzlwang@outlook.com>
2024-03-04 14:41:14 -0800

    imported fragment

---
+++ Assets/Scripts/Cards/EnemyCards/BeetleCards/Fragment.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+
+public class Fragment : ActionClass
+{
+    public override void ExecuteActionEffect()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    public override void Initialize()
+    {
+        base.Initialize();
+        lowerBound = 2;
+        upperBound = 3;
+        
+        Speed = 5;
+
+        description = "If this card hits a player, gain +1 resonate";
+
+        myName = "Fragment";
+        CardType = CardType.MeleeAttack;
+        Renderer renderer = GetComponent<Renderer>();
+        ogMaterial = renderer.material; // og sprite of card
+        OriginalPosition = transform.position;
+    }
+
+
+
+    public override void OnHit()
+    {
+        // TODO: give resonate on hit
+    }
+}
