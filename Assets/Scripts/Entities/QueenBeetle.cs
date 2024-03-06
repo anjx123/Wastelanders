6118fcc41ac772fd4a4a4971c5097529cc9d2486
Andrew Wang <andrewzlwang@outlook.com>
2024-03-05 19:31:44 -0800

    beetles now transfer buffs to queen!

--- Assets/Scripts/Entities/QueenBeetle.cs
+++ Assets/Scripts/Entities/QueenBeetle.cs
@@ -15,7 +15,16 @@ public class QueenBeetle : EnemyClass
         MaxHealth = 30;
         Health = MaxHealth;
         myName = "The Queen";
+
+        Beetle.OnGainBuffs += HandleGainedBuffs;
     }
 
-
+    // event handler for Beetle.OnGainBuffs. This is called whenever a beetle tries to
+    // add a buff.
+    // Adds the stacks that were directed to the beetle to the Queen instead.
+    private void HandleGainedBuffs(string buffType, int stacks)
+    {
+        AddStacks(buffType, stacks);
+        Debug.Log("Queen received " + stacks + " stacks of " + buffType + ".");
+    }
 }
---
+++ Assets/Scripts/Managers/AddBuffTester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBuffTester : MonoBehaviour
+{
+    public List<GameObject> beetles = new();
+
+    void OnMouseDown()
+    {
+        beetles[UnityEngine.Random.Range(0, beetles.Count)].GetComponent<EntityClass>().AddStacks(Accuracy.buffName, 2);
+    }
+}
+
