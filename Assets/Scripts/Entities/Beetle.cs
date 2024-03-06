3ce8c1b6fe1096c67514e438aba8b2e1ef4a8db4
Andrew Wang <andrewzlwang@outlook.com>
2024-03-06 15:25:14 -0800

    beetles can attack crystals now

--- Assets/Scripts/Entities/Beetle.cs
+++ Assets/Scripts/Entities/Beetle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
@@ -25,6 +26,25 @@ public abstract class Beetle : EnemyClass
     }
 
     public override void AddAttack(List<PlayerClass> players) {
+        if (players.Count == 0) return; // if there are no players, no need to attack crystals
+        // add crystals to list of potential targets
+        List<EntityClass> targets = players.Cast<EntityClass>().ToList();
+        List<EntityClass> crystals = CombatManager.Instance.getCrystals().Cast<EntityClass>().ToList();
+        targets.AddRange(crystals);
 
+        for (int i = 0; i < 100; i++)
+        {
+            Debug.Log(Random.Range(0, targets.Count));
+        }
+        // rest is the same
+        pool[0].GetComponent<ActionClass>().Target = targets[Random.Range(0, targets.Count)];
+        BattleQueue.BattleQueueInstance.AddEnemyAction(pool[0].GetComponent<ActionClass>(), this);
+        combatInfo.SetCombatSprite(pool[0].GetComponent<ActionClass>());
+        combatInfo.GetComponentInChildren<CombatCardUI>().actionClass = pool[0].GetComponent<ActionClass>();
+        pool.RemoveAt(0);
+        if (pool.Count < 1)
+        {
+            Reshuffle();
+        }
     }
 }
