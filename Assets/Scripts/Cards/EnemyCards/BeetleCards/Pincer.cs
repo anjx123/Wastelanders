da415f0b786f650839fabf9d59a939a7f530db00
Andrew Wang <andrewzlwang@outlook.com>
2024-03-05 21:24:51 -0800

    implemented all three beetle types

---
+++ Assets/Scripts/Cards/EnemyCards/BeetleCards/Pincer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+using static Unity.Collections.AllocatorManager;
+using static UnityEngine.UI.Image;
+
+public class Pincer : BeetleAttacks
+{
+    public override void ExecuteActionEffect()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    public override void Initialize()
+    {
+        base.Initialize();
+        lowerBound = 1;
+        upperBound = 4;
+        
+        Speed = 3;
+
+        description = "Ouch!";
+
+        myName = "Pincer";
+        CardType = CardType.MeleeAttack;
+        Renderer renderer = GetComponent<Renderer>();
+        ogMaterial = renderer.material; // og sprite of card
+        OriginalPosition = transform.position;
+    }
+
+
+
+    public override void OnHit()
+    {
+        
+    }
+}
