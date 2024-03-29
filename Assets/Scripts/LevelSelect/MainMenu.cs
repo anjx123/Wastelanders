ad507e302a715e4787de768ede54cb64d857aa7b
anjx123 <114792511+anjx123@users.noreply.github.com>
2024-03-28 21:35:14 -0700

    Main Menu Buff

--- Assets/Scripts/LevelSelect/MainMenu.cs
+++ Assets/Scripts/LevelSelect/MainMenu.cs
@@ -6,11 +6,7 @@ using UnityEngine.UI;
 
 public class MainMenu : LevelSelect
 {
-    protected override void Awake()
-    {
-        base.Awake();
-    }
-
+    [SerializeField] private GameObject wastelandersText;
     public void QuitGame()
     {
         #if UNITY_EDITOR
@@ -18,5 +14,27 @@ public class MainMenu : LevelSelect
         #endif
         Application.Quit();
 
+    }
+
+
+    private float cycleScaling = 2f; // Higher the number, the faster one phase is 
+    private float bobbingAmount = 0.1f; //Amplitude
+    private float timer = 0;
+    private float verticalOffset = 0;
+
+    //Makes the game over text bob up and down!
+    void Update()
+    {
+        float previousOffset = verticalOffset;
+        float waveslice = Mathf.Sin(cycleScaling * timer);
+        timer += Time.deltaTime;
+        if (timer > Mathf.PI * 2)
+        {
+            timer = timer - (Mathf.PI * 2);
+        }
+
+        verticalOffset = waveslice * bobbingAmount;
+        float translateChange = verticalOffset - previousOffset;
+        wastelandersText.transform.position = new Vector3(wastelandersText.transform.position.x, wastelandersText.transform.position.y + translateChange, wastelandersText.transform.position.z);
     }
 }
