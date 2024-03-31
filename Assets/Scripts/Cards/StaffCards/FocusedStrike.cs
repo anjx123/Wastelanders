aaf1e31116d2229ace77c4c4cba89b07bf154b15
anjx123 <114792511+anjx123@users.noreply.github.com>
2024-03-31 14:13:33 -0700

    Focused Strike Balance change

--- Assets/Scripts/Cards/StaffCards/FocusedStrike.cs
+++ Assets/Scripts/Cards/StaffCards/FocusedStrike.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class FocusedStrike : StaffCards
 {
-    public override void OnCardStagger()
-    {
-        Debug.Log("Executing Effect");
-    }
-
     // Start is called before the first frame update
     public override void Initialize()
     {
         CardType = CardType.MeleeAttack;
         myName = "Focused Strike";
-        description = "Double your Flow then make this attack. If unstaggered, gain 1 Flow.";
+        description = "Double your Flow then make this attack. On hit, gain 1 Flow.";
         lowerBound = 2;
         upperBound = 2;
         Speed = 2;
@@ -28,9 +23,9 @@ public class FocusedStrike : StaffCards
         base.ApplyEffect();
     }
 
-    public override void CardIsUnstaggered()
+    public override void OnHit()
     {
-        base.CardIsUnstaggered();
+        base.OnHit();
         Origin.AddStacks(Flow.buffName, 1);
     }
 
