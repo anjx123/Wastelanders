f73f4727f8d56e22c945cb14fc772eed076c2f07
anjx123 <114792511+anjx123@users.noreply.github.com>
2025-06-03 23:48:41 -0700

    Combat fade screen handler rework

--- Assets/Scripts/Managers/CombatManager.cs
+++ Assets/Scripts/Managers/CombatManager.cs
@@ -31,6 +31,8 @@ public class CombatManager : MonoBehaviour
     
     [SerializeField]
     private SpriteRenderer fadeScreen;
+    private FadeScreenHandler fadeScreenHandler;
+
     bool fadeActive = false;
 
     [SerializeField] private PlayerDatabase playerDatabase;
@@ -106,6 +108,8 @@ public class CombatManager : MonoBehaviour
     void Start()
     {
         GameState = GameState.GAME_START; //Put game start code in the performGameStart method.
+        fadeScreenHandler = gameObject.AddComponent<FadeScreenHandler>();
+        fadeScreenHandler.SetFadeScreen(fadeScreen);
         screenShakeHandler = gameObject.AddComponent<ScreenShakeHandler>();
         screenShakeHandler.DynamicCamera = dynamicCamera;
         ActionClass.CardStateChange += HandleCrosshairEnemies;
@@ -317,13 +321,14 @@ public class CombatManager : MonoBehaviour
 
     public IEnumerator FadeInLightScreen(float duration)
     {
-        yield return StartCoroutine(FadeBackground(1f, 0f, duration));
+        yield return StartCoroutine(fadeScreenHandler.FadeInLightScreen(duration));
     }
 
     public IEnumerator FadeInDarkScreen(float duration)
     {
-        yield return StartCoroutine(FadeBackground(0f, 1f, duration));
+        yield return StartCoroutine(fadeScreenHandler.FadeInDarkScreen(duration));
     }
+
     private void PerformOutOfCombat()
     {
         Deactivate(handContainer);
@@ -385,39 +390,13 @@ public class CombatManager : MonoBehaviour
     //set (@param darkenScene) true to fade **combat background** in, false to fade out
     private IEnumerator FadeCombatBackground(bool darkenScene)
     {
-
-        float startValue = fadeScreen.color.a;
         float duration = 1f;
-
-        float endValue;
-        if (darkenScene)
-        {
-            endValue = 0.8f;
-        }
+        if (darkenScene) 
+            yield return StartCoroutine(fadeScreenHandler.FadeInAlpha(0.8f, duration));
         else
-        {
-            startValue = Mathf.Max(fadeScreen.color.a - 0.3f, 0f); //Clamped to prevent visual nausua with strange alpha change
-            endValue = 0f;
-        }
-
-        yield return StartCoroutine(FadeBackground(startValue, endValue, duration));
+            yield return StartCoroutine(fadeScreenHandler.FadeInLightScreen(duration));
     }
-    //Fade Background that gives you more control over the level of fade
-    private IEnumerator FadeBackground(float startAlpha, float endAlpha, float duration)
-    {
-        if (fadeActive) yield break;
-        fadeActive = true;
-        float elapsedTime = 0f;
 
-        while (elapsedTime < duration)
-        {
-            elapsedTime += Time.deltaTime;
-            float newAlpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / duration);
-            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, newAlpha);
-            yield return null;
-        }
-        fadeActive = false;
-    }
     private void CrosshairAllEnemies()
     {
         foreach (var enemy in enemyTeam)
