eb162b15feb33931bf9282a1578b3a83e54c7c31
anjx123 <114792511+anjx123@users.noreply.github.com>
2025-07-25 23:47:22 -0700

    Cleaned up Scene Initializer

--- Assets/Scripts/SceneBuilder/SceneInitializer.cs
+++ Assets/Scripts/SceneBuilder/SceneInitializer.cs
@@ -8,11 +8,8 @@ using System;
 public class SceneInitializer : MonoBehaviour
 {
     public static SceneInitializer Instance { get; private set; }
-
-    [Header("Manager Registry")]
-    [Tooltip("A list of all possible manager prefabs that can be instantiated by this system.")]
-    [SerializeField] private List<MonoBehaviour> managerPrefabs = new List<MonoBehaviour>();
-    private Dictionary<Type, MonoBehaviour> _managerPrefabMap;
+    [SerializeField] private SceneInitializerPrefabs initializablePrefabs;
+    public SceneInitializerPrefabs InitializablePrefabs => initializablePrefabs;
 
     public void Awake()
     {
@@ -26,51 +23,22 @@ public class SceneInitializer : MonoBehaviour
         InitializeManagersForScene();
     }
 
-    public T GetPrefab<T>() where T : MonoBehaviour
-    {
-        _managerPrefabMap.TryGetValue(typeof(T), out var manager);
-        return manager as T;
-    }
-
     private void InitializeManagersForScene()
     {
-        _managerPrefabMap = managerPrefabs.ToDictionary(p => p.GetType(), p => p);
-
         string currentSceneName = SceneManager.GetActiveScene().name;
-        SceneData sceneData = SceneData.FromSceneName(currentSceneName);
-
-        if (sceneData == null)
+        var requiredManagers = SceneData.FromSceneName(currentSceneName).RequiredPrefabs(initializablePrefabs);
+        var managersParent = new GameObject("[Managers]");
+        
+        foreach (var managerPrefab in requiredManagers)
         {
-            Debug.LogWarning($"No SceneData blueprint found for scene '{currentSceneName}'. No managers will be auto-instantiated.");
-            return;
-        }
-
-        var requiredManagers = sceneData.RequiredManagerTypes;
-        if (requiredManagers == null || !requiredManagers.Any()) return;
-
-        var managersParent = GameObject.Find("[Managers]");
-        if (managersParent == null)
-        {
-            managersParent = new GameObject("[Managers]");
-        }
-
-        foreach (var managerType in requiredManagers)
-        {
-            if (FindObjectOfType(managerType) != null)
-            {
-                continue;
-            }
-
-            if (_managerPrefabMap.TryGetValue(managerType, out var managerPrefab))
-            {
-                var newManagerInstance = Instantiate(managerPrefab, managersParent.transform);
-                newManagerInstance.name = managerPrefab.name;
-            }
-            else
-            {
-                Debug.LogError($"Scene '{currentSceneName}' requires a manager of type '{managerType.Name}', " +
-                               $"but it was not found in the SceneInitializer's manager registry.", this);
-            }
+            var newManagerInstance = Instantiate(managerPrefab, managersParent.transform);
+            newManagerInstance.name = managerPrefab.name;
         }
     }
+}
+
+[Serializable]
+public class SceneInitializerPrefabs
+{
+    public AudioManager audioManager;
 }
