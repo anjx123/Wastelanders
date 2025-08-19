a25bd2c514a86caf87590754824265dcf4c98414
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-08-18 17:39:49 -0700

    ives run moment

--- Assets/Scripts/Dialogue/PreBounty/PreBounty_1.cs
+++ Assets/Scripts/Dialogue/PreBounty/PreBounty_1.cs
@@ -2,7 +2,6 @@ using LevelSelectInformation;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.ConstrainedExecution;
 using Systems.Persistence;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -20,7 +19,8 @@ public class PreBounty_1 : MonoBehaviour
     
     [SerializeField] private List<DialogueText> bountyDiscussionDialogue;
 
-
+    [SerializeField] private float ivesMoveSpeed = 2f;
+    
     public void Start()
     {
         StartCoroutine(StartScene());
@@ -32,7 +32,52 @@ public class PreBounty_1 : MonoBehaviour
         yield return new WaitForSeconds(1f);
         
         yield return fadeScreenHandler.FadeInLightScreen(1f);
-        
-        ives.GetComponent<Animator>().SetBool("IsMoving", true);
+
+        yield return MoveCharacterToTarget(ives, ivesTarget, ivesMoveSpeed);
     }
+
+    private IEnumerator MoveCharacterToTarget(GameObject character, Transform target, float speed)
+    {
+        Animator animator = character.GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.SetBool("IsMoving", true);
+        }
+
+        Vector3 originalScale = character.transform.localScale;
+
+        while (Vector3.Distance(character.transform.position, target.position) > 0.05f)
+        {
+            // Determine direction
+            float direction = target.position.x - character.transform.position.x;
+
+            // Flip character by adjusting scale
+            if (direction != 0)
+            {
+                character.transform.localScale = new Vector3(
+                    Mathf.Sign(direction) * Mathf.Abs(originalScale.x), 
+                    originalScale.y, 
+                    originalScale.z
+                );
+            }
+
+            // Move character
+            character.transform.position = Vector3.MoveTowards(
+                character.transform.position,
+                target.position,
+                speed * Time.deltaTime
+            );
+
+            yield return null;
+        }
+
+        // Snap to exact position
+        character.transform.position = target.position;
+
+        if (animator != null)
+        {
+            animator.SetBool("IsMoving", false);
+        }
+    }
+
 }
