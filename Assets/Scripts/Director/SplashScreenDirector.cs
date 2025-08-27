035aa098d3b442d1f28736de1533c09fda161e2d
Andrew Wang <115036225+drew-gnaw@users.noreply.github.com>
2025-08-27 16:29:07 -0700

    Director

---
+++ Assets/Scripts/Director/SplashScreenDirector.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+namespace Director
+{
+    public class SplashScreenDirector : MonoBehaviour
+    {
+        [SerializeField] private VideoPlayer videoPlayer;
+        [SerializeField] private FadeScreenHandler fadeScreenHandler;
+
+        private void Start()
+        {
+            videoPlayer.loopPointReached += OnVideoEnd;
+            StartCoroutine(StartSequence());
+        }
+
+        private IEnumerator StartSequence()
+        {
+            videoPlayer.Play();
+            yield return null;
+        }
+
+        private void OnVideoEnd(VideoPlayer vp)
+        {
+            StartCoroutine(EndSequence());
+        }
+
+        private IEnumerator EndSequence()
+        {
+            yield return new WaitForSeconds(2f);
+            yield return fadeScreenHandler.FadeInDarkScreen(1f);
+            yield return new WaitForSeconds(0.5f);
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
