9f05e4fe6cd916eff6c9c27664276ee01f89b4a3
anjx123 <114792511+anjx123@users.noreply.github.com>
2025-11-23 19:22:09 -0800

    Cleaned up Syntax

--- Assets/Scripts/DialogueScripts/DialogueEvents.cs
+++ Assets/Scripts/DialogueScripts/DialogueEvents.cs
@@ -11,8 +11,7 @@ namespace DialogueScripts
 
     public class SetSpeaker: DialogueEvents, IEvent
     {
-        [SerializeField] private CharacterActor actor = CharacterActor.Jackie;
-        public CharacterActor Actor => actor;
+        public CharacterActor actor = CharacterActor.Jackie;
         public override void Execute() => this.Invoke();
     }
 
@@ -26,23 +25,16 @@ namespace DialogueScripts
 
     public class SpriteChange : DialogueEvents, IEvent
     {
-        [SerializeField] private CharacterActor actor = CharacterActor.Jackie;
-        [SerializeField] private Sprite sprite;
-        public CharacterActor Actor => actor;
-        public Sprite Sprite => sprite;
-
+        public CharacterActor actor = CharacterActor.Jackie;
+        public Sprite sprite;
         public override void Execute() => this.Invoke();
     }
 
-
     public class ActorAction : DialogueEvents, IEvent
     {
-        [SerializeField] private CharacterActor actor = CharacterActor.Jackie;
-        [SerializeField] private CharacterActions action = CharacterActions.SetLeft;
-        [SerializeField] private float duration = 1.0f;
-        public CharacterActor Actor => actor;
-        public CharacterActions Action => action;
-        public float Duration => duration;
+        public CharacterActor actor = CharacterActor.Jackie;
+        public CharacterActions action = CharacterActions.SetLeft;
+        public float duration = 1.0f;
         public override void Execute() => this.Invoke();
     }
 
@@ -60,8 +52,7 @@ namespace DialogueScripts
     /// Change the vertical positioning of the dialogue box. 
     public class VerticalLayoutChange : DialogueEvents, IEvent
     {
-        [SerializeField] private Layout layout = Layout.Lower;
-        public Layout Layout => layout;
+        public Layout Layout = Layout.Lower;
         public override void Execute() => this.Invoke();
     }
 
@@ -74,8 +65,7 @@ namespace DialogueScripts
     /// Automatically advances this dialogue entry after it finishes. 
     public class AutoAdvanceAfter : DialogueEvents, IEvent
     {
-        [SerializeField] private float time = 0.5f;
-        public float Time => time;
+        public float Time = 0.5f;
         public override void Execute() => this.Invoke();
     }
 }
--- Assets/Scripts/DialogueScripts/StageDirector.cs
+++ Assets/Scripts/DialogueScripts/StageDirector.cs
@@ -29,44 +29,35 @@ namespace DialogueScripts
             this.Subscribe<SetSpeaker>(OnSetSpeaker);
         }
 
+
         private void OnSetSpeaker(SetSpeaker ss)
         {
-            foreach (var kvp in activeActors)
+            foreach ((CharacterActor characterProfile, DialogueActor actorInstance) in activeActors)
             {
-                CharacterActor characterProfile = kvp.Key;
-                DialogueActor actorInstance = kvp.Value;
-
-                bool isSpeaking = characterProfile == ss.Actor;
-
-                actorInstance.SetSpeaker(isSpeaking);
-
-                if (isSpeaking)
-                {
-                    actorInstance.transform.SetAsLastSibling();
-                }
+                actorInstance.SetSpeaker(isSpeaking: characterProfile == ss.actor);
             }
         }
 
         private void OnActorAction(ActorAction mc)
         {
-            var actor = GetOrSummonActor(mc.Actor);
+            var actor = GetOrSummonActor(mc.actor);
 
-            _ = mc.Action switch
+            _ = mc.action switch
             {
-                CharacterActions.SetLeft => actor.MoveTo(leftPos.position, mc.Duration),
-                CharacterActions.SetRight => actor.MoveTo(rightPos.position, mc.Duration),
-                CharacterActions.SetMiddle => actor.MoveTo(centerPos.position, mc.Duration),
-                CharacterActions.SetOffscreenLeft => actor.MoveTo(offScreenLeft.position, mc.Duration),
-                CharacterActions.SetOffscreenRight => actor.MoveTo(offScreenRight.position, mc.Duration),
-                CharacterActions.FadeIn => actor.FadeActor(true, mc.Duration),
-                CharacterActions.FadeOut => actor.FadeActor(false, mc.Duration),
-                _ => throw new ArgumentOutOfRangeException(nameof(mc.Action), mc.Action, "Unhandled CharacterAction")
+                CharacterActions.SetLeft => actor.MoveTo(leftPos.position, mc.duration),
+                CharacterActions.SetRight => actor.MoveTo(rightPos.position, mc.duration),
+                CharacterActions.SetMiddle => actor.MoveTo(centerPos.position, mc.duration),
+                CharacterActions.SetOffscreenLeft => actor.MoveTo(offScreenLeft.position, mc.duration),
+                CharacterActions.SetOffscreenRight => actor.MoveTo(offScreenRight.position, mc.duration),
+                CharacterActions.FadeIn => actor.FadeActor(true, mc.duration),
+                CharacterActions.FadeOut => actor.FadeActor(false, mc.duration),
+                _ => throw new ArgumentOutOfRangeException(nameof(mc.action), mc.action, "Unhandled CharacterAction")
             };
         }
 
         private void OnSpriteChange(SpriteChange sc)
         {
-            GetOrSummonActor(sc.Actor).ChangeSprite(sc.Sprite);
+            GetOrSummonActor(sc.actor).ChangeSprite(sc.sprite);
         }
 
         private DialogueActor GetOrSummonActor(CharacterActor actor)
